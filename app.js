5a61a4a87f0866801154180c15e21bdbbb3a9cd5
Stuart P. Bentley <stuart@testtrack4.com>
2013-06-07 16:31:10 -0400

    Add app files
    Normally I'd gitignore node_modules, but I'm going to try committing it, see how it goes.

---
+++ app.js
@@ -0,0 +1,60 @@
+var express = require('express');
+var twilio = require('twilio');
+
+function playTones(twiml,tones){
+  
+  //allow array, string, and number input
+  if(!Array.isArray(tones)){
+    tones = tones.toString().split('');
+  }
+  
+  for(var i = 0; i < tones.length; tones++){
+    var tone = tones[i];
+    twiml.play('/dtmf/'
+      + (tone == '#' ? 'pound' : tone == '*' ? 'star' : tone)
+      + '.wav');
+  }
+  
+  return twiml;
+}
+
+function playToneResponse(res,tones){
+  res.type('text/xml').send(
+    playTones(new twilio.TwimlResponse(), tones)
+      .toString());
+}
+
+module.exports = function(cfg){
+  var app = express();
+  
+  app.use(express.bodyParser());
+  app.use(express.favicon());
+  app.use(express.static('/dtmf',__dirname+'/dtmf'));
+  
+  app.get('/incoming',function(req,res){
+    // respond with tones for verifying number to Google Voice
+    if(process.env.RESPOND_TONES) {
+      playToneResponse(res,process.env.RESPOND_TONES);
+    } else {
+      res.type('text/xml').send(new twilio.TwimlResponse()
+        .gather({
+          timeout: process.env.PASSCODE_ENTRY_TIMEOUT || 5
+        }, function(twiml){
+          twiml.say('Enter the passcode, followed by pound');
+        }).toString());
+    }
+  });
+
+  app.post('/incoming',function(req,res){
+    console.log(req.body);
+    if (req.body.Digits == process.env.PASSCODE_DIGITS) {
+      playToneResponse(res,9);
+    } else {
+      res.type('text/xml').send(new twilio.TwimlResponse()
+        .say('Passcode not recognized')
+        .toString());
+    }
+  });
+  
+  return app;
+};
