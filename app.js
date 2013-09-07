e599a1ec046c4a1223a44b853d4633524a704d06
Stuart P. Bentley <stuart@testtrack4.com>
2013-09-07 00:41:27 -0400

    Refactor to keep config out of the db

--- app.js
+++ app.js
@@ -35,13 +35,6 @@ module.exports = function appctor(cfg) {
     {no_ready_check: true});
   db.auth(cfg.redis.password);
 
-  // Create an object, matching a list of keys to a list of values.
-  function objectify(fields,values) {
-    var hfo = {};
-    for (var i = 0; i < fields.length; i++) hfo[fields[i]] = values[i];
-    return hfo;
-  }
-
   // Create the app
   var app = express();
 
@@ -65,220 +58,162 @@ module.exports = function appctor(cfg) {
     res.render('google-voice-setup.jade');
   });
 
-  // The list of fields that go straight from the configuration form
-  // to the database.
-  var configvars = ['smsttl', 'prompt', 'sleep', 'unlockTone',
-    'gatherTimeout', 'finishOnKey', 'numDigits'];
+  // The route for recieving a call from Twilio.
+  app.post('/voice',function(req, res, next){
+    var unlockToken = req.query.unlockToken;
+    var unlockTone = req.query.unlockTone || '9';
+    var sleep = req.query.sleep;
+    var gatherTimeout = req.query.gatherTimeout || 5;
+    var finishOnKey = req.query.finishOnKey || '';
+    var numDigits = req.query.numDigits;
+    var prompt = req.query.prompt || 'Enter the passcode';
 
-  // The route receiving the submission form for configuration.
-  app.post('/',function(req,res,next) {
-    // Bcrypt the passcode with 10 rounds
-    bcrypt.hash(req.body.passcode, 10, function(err, passhash) {
+    function respond(err, unlock) {
       if (err) return next(err);
 
-      // The parameters of the HMSET operation:
-      var hfields = [
-        // The key we're setting to the hash object,
-        req.body.accsid + req.body.number + ' config',
-        // the key/value for the passcode's bcrypt hashsum
-        'passhash', passhash
-      ];
+      // Create our TwiML response.
+      var resTwiml = new twilio.TwimlResponse();
 
-      // Add all the other fields from the config form to the HMSET operation
-      // parameters as key/value pairs
-      for (var i=0; i < configvars.length; i++) {
-        hfields.push(configvars[i]);
-        hfields.push(req.body[configvars[i]]);
+      // If this client is configured to sleep before answering the call,
+      // insert a pause at the top of the response so Twilio will wait.
+      if (sleep) resTwiml.pause(sleep);
+
+      // If the door is currently "unlocked"
+      if (unlock) {
+
+        // Re-lock the door by deleting the unlock record
+        db.del('unlock/'+unlockToken, function (err, status) {
+          if (err) return next(err);
+          //if no err let's just assume the status is OK or otherwise fine
+
+          // Once we've gotten confirmation that the door re-lock has
+          // completed successfully and without errors, play the tone
+          // sequence that unlocks the door
+          playTones(resTwiml,unlockTone);
+          res.type('text/xml').send(resTwiml.toString());
+        });
+
+      // If there's no "unlock" record stating that this call should bypass
+      // passcode entry and unlock immediately
+      } else {
+
+        // The options/attributes of the Gather TwiML verb that gathers
+        // the passcode
+        var gatherattrs = {
+
+          // The time to wait before gathering. Defaults to 5 seconds.
+          timeout: gatherTimeout,
+
+          // A key to mark as ending the sequence (by default, only wait)
+          finishOnKey: finishOnKey,
+
+          // Where to POST the results to.
+          action: '/digits' + require('url').parse(req.url).query
+        };
+
+        // If there's a maximum number of digits set to gather,
+        // add that maximum to the Gather verb attributes
+        if (numDigits) gatherattrs.numDigits = numDigits;
+
+        // Add the Gather prompt to the response
+        resTwiml.gather(gatherattrs, function (twiml) {
+          twiml.say(prompt);
+        });
       }
 
-      // Set the config object in the database
-      db.hmset(hfields, function(err,status) {
-        if (err) return next(err);
-        //if no err let's just assume the status is OK or otherwise fine
-        else return res.redirect('/success');
-      });
-    });
-  });
-
-  // The route for recieving a call from Twilio.
-  app.post('/incoming/voice',function(req,res, next){
-
-    // The combined ID to identify this client.
-    var cid = req.body.AccountSid + req.body.To;
-
-    // The fields we need from the database, for this client,
-    // to respond to the first phase of the call.
-    var fields = ['sleep', 'prompt', 'unlockTone',
-      'gatherTimeout', 'finishOnKey', 'numDigits'];
+      // Send the response we've built to Twilio
+      res.type('text/xml').send(resTwiml.toString());
+    }
 
     // Get the configuration fields we need for our response from the database,
     // and check at the same time to see if the door is currently unlocked
     // (via SMS) to see if we should prompt for the passcode
-    db.multi()
-      .hmget([cid + ' config'].concat(fields))
-      .get(cid + ' unlocked')
-      .exec(function (err, dbres) {
-        if (err) return next(err);
-
-        // Create an object mapping the keys we asked for to the value list
-        // returned from the Redis request.
-        var hfo = objectify(fields, dbres[0]);
-
-        // Create our TwiML response.
-        var resTwiml = new twilio.TwimlResponse();
-
-        // If this client is configured to sleep before answering the call,
-        // insert a pause at the top of the response so Twilio will wait.
-        if (hfo.sleep) {
-          resTwiml.pause(hfo.sleep);
-        }
-
-        // If the door is currently "unlocked"
-        if (dbres[1]) {
-
-          // Re-lock the door by deleting the unlock record
-          db.del(cid + ' unlocked', function (err, status) {
-            if (err) return next(err);
-            //if no err let's just assume the status is OK or otherwise fine
-
-            // Once we've gotten confirmation that the door re-lock has
-            // completed successfully and without errors, play the tone
-            // sequence that unlocks the door
-            playTones(resTwiml,hfo.unlockTone);
-          });
-
-        // If there's no "unlock" record stating that this call should bypass
-        // passcode entry and unlock immediately
-        } else {
-
-          // The options/attributes of the Gather TwiML verb that gathers
-          // the passcode
-          var gatherattrs = {
-
-            // The time to wait before gathering. Defaults to 5 seconds.
-            timeout: hfo.gatherTimeout || 5,
-
-            // A key to mark as ending the sequence (by default, only wait)
-            finishOnKey: hfo.finishOnKey || '',
-
-            // Where to POST the results to.
-            action: '/incoming/voice/digits'
-          };
-
-          // If there's a maximum number of digits set to gather,
-          // add that maximum to the Gather verb attributes
-          if (hfo.numDigits) gatherattrs.numDigits = hfo.numDigits;
-
-          // Add the Gather prompt to the response
-          resTwiml.gather(gatherattrs, function (twiml) {
-            twiml.say(hfo.prompt || 'Enter the passcode');
-          });
-        }
-
-        // Send the response we've built to Twilio
-        res.type('text/xml').send(resTwiml.toString());
-      });
+    if (unlockToken) db.get('unlock/'+unlockToken, respond);
+    else respond();
   });
 
   // The route for responding to passcodes.
-  app.post('/incoming/voice/digits', function (req, res, next) {
+  app.post('/digits', function (req, res, next) {
 
-    // The combined ID to identify this client.
-    var cid = req.body.AccountSid + req.body.To;
+    var bcryptPasshash = req.query.bcryptPasshash;
+    var passcode = req.query.passcode;
+    var unlockTone = req.query.unlockTone || '9';
 
-    // Get the hash of the passcode, and the tone sequence to play if the
-    // passcode sent matches the stored hashed passcode, from the database
-    db.hmget([cid + ' config', 'passhash', 'unlockTone'],
-      function (err, hvalues) {
-
+    function evaluate(err,passmatch) {
       if (err) return next(err);
 
-      // Match the stored passcode (or, if no client was found with the
-      // given credentials, compare an invalid passcode) with the entered
-      // passcode
-      bcrypt.compare(req.body.Digits, hvalues[0] || impossibleHash,
-        function (err, passmatch) {
+      // Create our TwiML response.
+      var resTwiml = new twilio.TwimlResponse();
 
-        if (err) return next(err);
+      // If the passcode matches
+      if (passmatch) {
 
-        // Create our TwiML response.
-        var resTwiml = new twilio.TwimlResponse();
+        // Play the tone that unlocks the door
+        playTones(resTwiml, unlockTone);
 
-        // If the passcode matches
-        if (passmatch) {
+      // If the passcode didn't match
+      } else {
 
-          // Play the tone that unlocks the door
-          playTones(resTwiml, hvalues[1]);
+        // Tell the user what passcode was heard, and that it was
+        // not recognized as correct
+        resTwiml.say('Passcode '
+          // (inserting a space between each digit so Twilio reads it
+          // as a sequence of digits rather than one number)
+          + req.body.Digits.split('').join(' ')
+          + ' not recognized');
+      }
 
-        // If the passcode didn't match
-        } else {
+      // Send the response we've built to Twilio
+      res.type('text/xml').send(resTwiml.toString());
+    }
 
-          // Tell the user what passcode was heard, and that it was
-          // not recognized as correct
-          resTwiml.say('Passcode '
-            // (inserting a space between each digit so Twilio reads it
-            // as a sequence of digits rather than one number)
-            + req.body.Digits.split('').join(' ')
-            + ' not recognized');
-        }
-
-        // Send the response we've built to Twilio
-        res.type('text/xml').send(resTwiml.toString());
-      });
-    });
+    if (bcryptPasshash) bcrypt.compare(req.body.Digits, evaluate);
+    else evaluate(null,passcode == req.body.Digits);
   });
 
   // The route for responding to SMS messages.
-  app.post('/incoming/sms', function (req,res,next) {
+  app.post('/sms', function (req, res, next) {
 
-    // The combined ID to identify this client.
-    var cid = req.body.AccountSid + req.body.To;
+    var bcryptPasshash = req.query.bcryptPasshash;
+    var unlockToken = req.query.unlockToken;
+    var passcode = req.query.passcode;
+    var ttl = req.query.ttl;
 
-    // Get the hash of the passcode and the duration for successful
-    // SMS unlocks from the database
-    db.hmget([cid + ' config', 'passhash', 'smsttl'],
-      function(err,hvalues) {
-
+    function evaluate(err,passmatch) {
       if (err) return next(err);
 
-      // Match the stored passcode (or, if no client was found with the
-      // given credentials, compare an invalid passcode) with the entered
-      // passcode
-      bcrypt.compare(req.body.Body, hvalues[0] || impossibleHash,
-        function (err, passmatch) {
+      // If the passcode matches
+      if (passmatch) {
 
-        if (err) return next(err);
+        // Set a record in the database marking that the door is unlocked,
+        // expiring after the unlock duration is up
+        db.setex('unlock/'+unlockToken, ttl, req.body.From,
+          function (err, status) {
 
-        // If the passcode matches
-        if (passmatch) {
+          if (err) return next(err);
+          //if no err let's just assume the status is OK or otherwise fine
 
-          // Set a record in the database marking that the door is unlocked,
-          // expiring after the unlock duration is up
-          db.setex(cid + ' unlocked', hvalues[1], req.body.From,
-            function (err, status) {
+          // Respond to the text message by telling the texter how long the
+          // door will be waiting to open
+          else return res.type('text/plain').send(
+            'Door will unlock automatically in the next '
+              + ttl + ' seconds');
+        });
 
-            if (err) return next(err);
-            //if no err let's just assume the status is OK or otherwise fine
+      // If the passcode doesn't match
+      } else {
+        // Echo the text body (or the first 100 characters of it if it was
+        // really long) back to the user and tell them it wasn't recognized
+        // as the correct passcode
+        res.type('text/plain').send(
+          'Passcode "' + req.body.Body.slice(0,100)
+            + (req.body.Body.length > 100 ? '...' : '')
+            + '" not recognized');
+      }
+    }
 
-            // Respond to the text message by telling the texter how long the
-            // door will be waiting to open
-            else return res.type('text/plain').send(
-              'Door will unlock automatically in the next '
-                + hvalues[1] + ' seconds');
-          });
-
-        // If the passcode doesn't match
-        } else {
-          // Echo the text body (or the first 100 characters of it if it was
-          // really long) back to the user and tell them it wasn't recognized
-          // as the correct passcode
-          res.type('text/plain').send(
-            'Passcode "' + req.body.Body.slice(0,100)
-              + (req.body.Body.length > 100 ? '...' : '')
-              + '" not recognized');
-        }
-      });
-    });
+    if (bcryptPasshash) bcrypt.compare(req.body.Body, evaluate);
+    else evaluate(null,passcode == req.body.Body);
   });
 
   // Respond with a 404 code for any other requests
