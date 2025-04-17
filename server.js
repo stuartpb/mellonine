5785b132903f7bc12fe358494a46fba51be4a653
Stuart P. Bentley <stuart@testtrack4.com>
2025-04-17 02:41:11 +0000

    Use server shim instead of envigorate
    envigorate was not built for a world with non-constructable async functions

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 var cfg = require('envigor')();
 
-require('./app.js')(cfg).listen(cfg.port || 3000,function(){
-  console.log("Listening on port " + (cfg.port || '3000'));
-});
+(async function() {
+  const app = await require('./app.js')(cfg);
+  return app.listen(cfg.port || 3000,function() {
+    console.log("Listening on port " + (cfg.port || '3000'));
+  });
+})().error(console.error);
